8f95bd36ff9fecf469cfecba88dacceccff6e7b2
arayni <arayni.max@gmail.com>
2023-02-18 14:53:12 +0100

    Added build script and additional includes from the used libraries and removed binary from source code

---
+++ build.sh
@@ -0,0 +1,4 @@
+SOURCEMOD_SCRIPTING_PATH=/d/sourcemod-scripting
+
+# run the compiler
+$SOURCEMOD_SCRIPTING_PATH/spcomp -D ./ -i $SOURCEMOD_SCRIPTING_PATH/include -i ./include -o ./build/phoskin.smx ./phoskin.sp
